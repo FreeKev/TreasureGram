e19cb8c7564ca5b4efa60a6268465b4c0eebf8d6
Justin Castilla <pxlperfection@gmail.com>
2017-06-30 08:16:03 -0500

    added like button functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     url(r'^user/(\w+)/$', views.profile, name='profile'),
     url(r'^([0-9]+)/$', views.show, name="show"),
     url(r'^post_url/$', views.post_treasure, name="post_treasure"),
+    url(r'^like_treasure/$', views.like_treasure, name='like_treasure'),
     url(r'^$', views.index),
+    url(r'^login/$', views.login_view, name="login"),
+    url(r'^logout/$', views.logout_view, name="logout"),
+
 ]
